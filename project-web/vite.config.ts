da2353aa9687bd4bd87e95c737c5d66b039d5a9e
hyzhu <zhuhuiyu2499669698@163.com>
2024-11-07 20:18:07 +0800

    feat: 路由安装

--- project-web/vite.config.ts
+++ project-web/vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: '0.0.0.0', //解决“vite use `--host` to expose”  ,允许从外部访问开发服务器
+    port: 8080, //指定开发服务器运⾏的端⼝
+    open: true //在启动开发服务器时⾃动打开默认浏览器
+  },
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: resolve(__dirname, 'src')
+      }
+    ]
+  }
 })
