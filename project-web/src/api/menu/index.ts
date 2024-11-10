b29e84913065374224bdece05b44206f4ad8d8de
hyzhu <zhuhuiyu2499669698@163.com>
2024-11-10 17:16:29 +0800

    feat: 编辑、删除菜单接口对接

--- project-web/src/api/menu/index.ts
+++ project-web/src/api/menu/index.ts
@@ -10,7 +10,17 @@ export const getParentApi = () => {
 export const addApi = (parm: MenuType) => {
   return http.post('/api/sysMenu', parm)
 }
+
 //列表
 export const getListApi = () => {
   return http.get('/api/sysMenu/list')
+}
+//编辑
+export const editApi = (parm: MenuType) => {
+  return http.put('/api/sysMenu', parm)
+}
+
+//删除
+export const deleteApi = (menuId: string) => {
+  return http.delete(`/api/sysMenu/${menuId}`)
 }
