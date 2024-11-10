8ea93dde0158686b8aa30c76c3f9c0d5966cecd1
hyzhu <zhuhuiyu2499669698@163.com>
2024-11-10 17:12:29 +0800

    feat: 菜单列表页面制作

--- project-web/src/api/menu/index.ts
+++ project-web/src/api/menu/index.ts
@@ -10,3 +10,7 @@ export const getParentApi = () => {
 export const addApi = (parm: MenuType) => {
   return http.post('/api/sysMenu', parm)
 }
+//列表
+export const getListApi = () => {
+  return http.get('/api/sysMenu/list')
+}
